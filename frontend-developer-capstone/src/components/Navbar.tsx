9ee4783bbdb619c0be6ad2569e0f0ffe96a25c7b
Leonardo Resende <leonardo.resendelsr@gmail.com>
2023-12-21 20:02:11 -0300

    add mobile navigation

--- frontend-developer-capstone/src/components/Navbar.tsx
+++ frontend-developer-capstone/src/components/Navbar.tsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
       <nav className="flex flex-row items-center justify-between py-4 px-5 sm:px-20 bg-white">
@@ -53,7 +57,91 @@ const Navbar = () => {
         </div>
 
         {/* Mobile Navigation Menu */}
-        {/* Code here... */}
+        <div className="lg:hidden">
+          <Bars3Icon
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+            className="h-10 w-10 text-black cursor-pointer"
+          />
+          {isOpen && (
+            <>
+              {/* Overlay to block content below */}
+              <div
+                className="fixed top-0 right-0 bottom-0 left-0 bg-black opacity-50"
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                }}
+              ></div>
+              {/* Mobile navigation content */}
+              <div className="fixed top-0 right-0 left-1/4 md:left-1/2 bottom-0 bg-white p-4">
+                <XMarkIcon
+                  onClick={() => {
+                    setIsOpen(!isOpen);
+                  }}
+                  className="h-10 w-10 text-black cursor-pointer"
+                />
+
+                <div className="flex flex-col gap-2 mt-4 p-4">
+                  <Link
+                    className="mt-2 font-semibold leading-6 text-slate-950 hover:text-slate-600"
+                    to={"/"}
+                    onClick={() => {
+                      setIsOpen(!isOpen);
+                    }}
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    className="mt-2 font-semibold leading-6 text-slate-950 hover:text-slate-600"
+                    to={"/"}
+                    onClick={() => {
+                      setIsOpen(!isOpen);
+                    }}
+                  >
+                    About
+                  </Link>
+                  <Link
+                    className="mt-2 font-semibold leading-6 text-slate-950 hover:text-slate-600"
+                    to={"/"}
+                    onClick={() => {
+                      setIsOpen(!isOpen);
+                    }}
+                  >
+                    Menu
+                  </Link>
+                  <Link
+                    className="mt-2 font-semibold leading-6 text-slate-950 hover:text-slate-600"
+                    to={"/"}
+                    onClick={() => {
+                      setIsOpen(!isOpen);
+                    }}
+                  >
+                    Reservations
+                  </Link>
+                  <Link
+                    className="mt-2 font-semibold leading-6 text-slate-950 hover:text-slate-600"
+                    to={"/"}
+                    onClick={() => {
+                      setIsOpen(!isOpen);
+                    }}
+                  >
+                    Order Online
+                  </Link>
+                  <Link
+                    className="mt-2 font-semibold leading-6 text-slate-950 hover:text-slate-600"
+                    to={"/"}
+                    onClick={() => {
+                      setIsOpen(!isOpen);
+                    }}
+                  >
+                    LogIn
+                  </Link>
+                </div>
+              </div>
+            </>
+          )}
+        </div>
       </nav>
     </>
   );
