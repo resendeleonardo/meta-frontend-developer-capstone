83d8c1370389dd09ae66273b4f6c660c4720cef4
Leonardo Resende <leonardo.resendelsr@gmail.com>
2023-12-23 14:32:07 -0300

    add footer

--- frontend-developer-capstone/src/components/Footer.tsx
+++ frontend-developer-capstone/src/components/Footer.tsx
@@ -1,7 +1,40 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div className="bg-gray-300 h-3/4 py-16 px-5 sm:px-20 xl:px-72">
+        <div className="grid grid-cols-4 gap-10">
+          <div style={{ backgroundImage: "url('/restaurant.jpg')"}} className="h-80 bg-cover rounded-lg"></div>
+          <div className="mt-10">
+            <h3 className="text-primary-color font-bold">Doormat navigation</h3>
+            <ul className="mt-5 text-black">
+              <li>Home</li>
+              <li>About</li>
+              <li>Menu</li>
+              <li>Reservations</li>
+              <li>Order online</li>
+              <li>LogIn</li>
+            </ul>
+          </div>
+          <div className="mt-10">
+            <h3 className="text-primary-color font-bold">Contact</h3>
+            <ul className="mt-5 text-black">
+              <li>Address</li>
+              <li>Phone number</li>
+            </ul>
+          </div>
+          <div className="mt-10">
+            <h3 className="text-primary-color font-bold">Social media</h3>
+            <ul className="mt-5 text-black">
+              <li>Instagram</li>
+              <li>Facebook</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Footer;
